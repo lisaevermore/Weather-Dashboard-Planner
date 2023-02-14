c9a9420351e022ae3bf2d72ee392b81ea8f6eb00
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-02-14 17:16:44 +0000

    adding some js

---
+++ script.js
@@ -0,0 +1,27 @@
+let displayed = document.querySelector('.list-group')
+let listOfcitys = JSON.parse(localStorage.getItem('city'));
+
+var searchInput = $(".search-button")
+
+$(".search-button").on('click', function(){
+    var input = document.getElementById('search-input').value;
+
+     let listOfcitys = JSON.parse(localStorage.getItem('city'));
+
+    if (listOfcitys === null){
+        listOfcitys = [];
+        listOfcitys.push(input);
+        localStorage.setItem('listOfcitys', JSON.stringify(listOfcitys));
+    } else {
+        listOfcitys.push(input);
+        localStorage.setItem('listOfcitys', JSON.stringify(listOfcitys));
+
+    }
+});
+
+
+
+ for (let i = 0; i < listOfcitys.length; i++){
+    var listEl = $("<li>").text(listOfcitys[i])
+    displayed.append(listEl);
+ }
