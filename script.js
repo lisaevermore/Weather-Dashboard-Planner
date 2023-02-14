e9b407e12202c5f78772946a5765c78918412037
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-02-14 17:39:52 +0000

    on click function

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let displayed = document.querySelector('.list-group')
-let listOfcitys = JSON.parse(localStorage.getItem('city'));
+// let listOfcitys = JSON.parse(localStorage.getItem('city')) || [];
+
+
+
+
+
+
+// let listOfcitys = JSON.parse(localStorage.getItem('city'));
 
 var searchInput = $(".search-button")
 
 $(".search-button").on('click', function(){
     var input = document.getElementById('search-input').value;
+    let listOfcitys = JSON.parse(localStorage.getItem('city')) || [];
 
-     let listOfcitys = JSON.parse(localStorage.getItem('city'));
+    //  let listOfcitys = JSON.parse(localStorage.getItem('city'));
 
     if (listOfcitys === null){
         listOfcitys = [];
@@ -17,11 +25,16 @@ $(".search-button").on('click', function(){
         localStorage.setItem('listOfcitys', JSON.stringify(listOfcitys));
 
     }
+    for (let i = 0; i < listOfcitys.length; i++){
+        var listEl = $("<li>").text(listOfcitys[i])
+        displayed.append(listEl);
+     }
+   
 });
 
 
 
- for (let i = 0; i < listOfcitys.length; i++){
-    var listEl = $("<li>").text(listOfcitys[i])
-    displayed.append(listEl);
- }
+//  for (let i = 0; i < listOfcitys.length; i++){
+//     var listEl = $("<li>").text(listOfcitys[i])
+//     displayed.append(listEl);
+//  }
